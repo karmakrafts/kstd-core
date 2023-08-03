0506359e0287326440e930221823842e34b51983
KitsuneAlex <alexh@karmakrafts.dev>
2023-08-03 16:05:26 +0200

    Remove unused files & update .gitignore

--- format.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# @author Alexander Hinze
-# @since 08/06/2023
-
-/media/data/develop/llvm-project/build/bin/clang-format -i -style=file --verbose ./include/kstd/*.hpp
-/media/data/develop/llvm-project/build/bin/clang-format -i -style=file --verbose ./test/*.cpp
